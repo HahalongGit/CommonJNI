07195cbc5c40b77bea38e83b7b08796653059a1e
customView <729665268@qq.com>
2018-06-12 13:46:29 +0800

    创建JNI测试项目

---
+++ app/src/main/cpp/JniTest.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 龙龙同学 on 2018/6/12.
+// 添加 一个 测试 cpp ,并在CMakeLists.txt 中配置添加
+//
+
+#include "JniTest.h"
---
+++ app/src/main/cpp/JniTest.h
@@ -0,0 +1,8 @@
+//
+// Created by 龙龙同学 on 2018/6/12.
+//
+
+#ifndef COMMONJNI_JNITEST_H
+#define COMMONJNI_JNITEST_H
+
+#endif //COMMONJNI_JNITEST_H
